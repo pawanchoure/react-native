4bb1aa474f6efe6c3ceaefa084d87b67d321e32e
Pawan Choure <pawanchoure@gmail.com>
2021-08-15 16:42:14 +0530

    Commit

---
+++ HouseShare/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+ <Text>
+          Hello World Pawan
+        </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
